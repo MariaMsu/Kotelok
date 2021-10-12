f2037b1f320845a42dcf87e89036fe979f9b833b
MariaMsu <i.knaz@yandex.ru>
2021-10-12 23:56:46 +0300

    проверяю, что функции getNextWord() и setUserInput() вызываются последовательно

--- app/src/main/kotlin/com/designdrivendevelopment/kotelok/trainer/WriteTrainer.kt
+++ app/src/main/kotlin/com/designdrivendevelopment/kotelok/trainer/WriteTrainer.kt
@@ -9,12 +9,22 @@ class WriteTrainer(
     onlyNotLearned: Boolean = true,
 ) :
     TrainerBase(learnableWords, onlyNotLearned, WRITE_PROGRESS) {
+    var previousWordIsChecked = true
 
     public fun getNextWord(): LearnableWord {
+        if (!previousWordIsChecked) {
+            throw RuntimeException("Check the previous word before to get the nex one")
+        }
+        previousWordIsChecked = false
         return shuffledWords[this.currentIdx]
     }
 
     public fun setUserInput(userString: String): String {
+        if (previousWordIsChecked) {
+            throw RuntimeException("Try to check the same word two times")
+        }
+        previousWordIsChecked = true
+
         val learnableWorld = shuffledWords[this.currentIdx]
 
         // incorrect answer
