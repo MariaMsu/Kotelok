59b3f8520dfad8fa32be4409d3567aa1a505ffbe
andreyhoco <andreygud2013@gmail.com>
2021-10-16 00:40:44 +0300

    Добавил в Word поле language

--- app/src/main/kotlin/com/designdrivendevelopment/kotelok/entities/Word.kt
+++ app/src/main/kotlin/com/designdrivendevelopment/kotelok/entities/Word.kt
@@ -2,6 +2,7 @@ package com.designdrivendevelopment.kotelok.entities
 
 data class Word(
     val id: Long,
+    val language: Language,
     val writing: String,
     val definitions: List<WordDefinition>
 )
