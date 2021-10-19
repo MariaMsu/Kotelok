86a54361a7ebc82f08257acb83a184ea84f4630f
andreyhoco <andreygud2013@gmail.com>
2021-10-19 20:57:47 +0300

    Добавил LearnableDefinitionsRepository

---
+++ app/src/main/kotlin/com/designdrivendevelopment/kotelok/LearnableDefinitionsRepository.kt
@@ -0,0 +1,21 @@
+package com.designdrivendevelopment.kotelok
+
+import com.designdrivendevelopment.kotelok.entities.LearnableDefinition
+import java.util.Date
+
+interface LearnableDefinitionsRepository {
+    suspend fun getLearnableDefinitionsByDictionaryId(dictionaryId: Long): List<LearnableDefinition>
+
+    suspend fun getAllLearnableDefinitions(): List<LearnableDefinition>
+
+    suspend fun getLearnableDefinitionById(wordId: Long): LearnableDefinition
+
+    suspend fun getLearnableDefinitionsByRepeatDate(repeatDate: Date): LearnableDefinition
+
+    suspend fun getLearnableDefinitionsByDictionaryIdAndRepeatDate(
+        dictionaryId: Long,
+        repeatDate: Date
+    ): LearnableDefinition
+
+    suspend fun updateLearnableDefinition(wordDefinition: LearnableDefinition)
+}
