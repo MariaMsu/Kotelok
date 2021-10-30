eb17acd904318bdb4e926cc31ee80ab51def76cd
andreyhoco <andreygud2013@gmail.com>
2021-10-30 13:54:10 +0300

    Добавил поле language в WordDefinition.kt

--- app/src/main/kotlin/com/designdrivendevelopment/kotelok/entities/WordDefinition.kt
+++ app/src/main/kotlin/com/designdrivendevelopment/kotelok/entities/WordDefinition.kt
@@ -6,11 +6,12 @@ data class WordDefinition(
     val id: Long,
     val wordId: Long,
     val writing: String,
+    val language: Language,
     val partOfSpeech: PartOfSpeech,
     val transcription: String,
     val synonyms: List<String>,
     val mainTranslation: String,
     val otherTranslations: List<String>,
     val examples: List<ExampleOfDefinitionUse>,
-    val nextRepeatDate: Date,
+    val nextRepeatDate: Date
 )
