719d12798fbd482ac87d88dee20073e91a5c0aa2
andreyhoco <andreygud2013@gmail.com>
2021-10-30 14:25:21 +0300

    Изменил способ навигации между вкладками

--- app/src/main/kotlin/com/designdrivendevelopment/kotelok/MainActivity.kt
+++ app/src/main/kotlin/com/designdrivendevelopment/kotelok/MainActivity.kt
@@ -1,7 +1,6 @@
 package com.designdrivendevelopment.kotelok
 
 import android.os.Bundle
-import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.commit
@@ -9,68 +8,31 @@ import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
     private var bottomNavigationView: BottomNavigationView? = null
-
-    private val viewModel: MainViewModel by viewModels()
-    private var currentTab: String = DICTIONARIES_TAB
+    private val bottomNavigator: BottomNavigator by lazy {
+        (application as KotelokApplication).appComponent.bottomNavigator
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         initViews()
-        setupViewModel()
-        openFragment(
-            fragment = DictionariesFragment.newInstance(),
-            tag = DictionariesFragment.OPEN_DICTIONARIES_TAG,
-            transactionName = DICTIONARIES_TAB
-        )
-        setupOnBackStackListener()
+        bottomNavigator.subscribe(supportFragmentManager)
+        if (savedInstanceState == null) {
+            val item = bottomNavigationView?.menu?.findItem(R.id.dictionary_tab)
+            item?.isChecked = true
+            bottomNavigator.setDefaultTab(bottomNavigator.getTabByName(DICTIONARIES_TAB))
+        }
     }
 
     override fun onDestroy() {
-        super.onDestroy()
+        bottomNavigator.unsubscribe()
         clearViews()
+        super.onDestroy()
     }
 
-    private fun setupOnBackStackListener() {
-        val fm = supportFragmentManager
-        fm.addOnBackStackChangedListener {
-            when (currentTab) {
-                DICTIONARIES_TAB -> {
-                    if (fm.findFragmentByTag(DictionariesFragment.OPEN_DICTIONARIES_TAG) == null) {
-                        openFragment(
-                            fragment = DictionariesFragment.newInstance(),
-                            tag = DictionariesFragment.OPEN_DICTIONARIES_TAG,
-                            DICTIONARIES_TAB
-                        )
-                    }
-                }
-                RECOGNIZE_TAB -> {
-                    if (fm.findFragmentByTag(RecognizeFragment.OPEN_RECOGNIZE_TAG) == null) {
-                        openFragment(
-                            fragment = RecognizeFragment.newInstance(),
-                            tag = RecognizeFragment.OPEN_RECOGNIZE_TAG,
-                            transactionName = RECOGNIZE_TAB
-                        )
-                    }
-                }
-                PROFILE_TAB -> {
-                    if (fm.findFragmentByTag(ProfileFragment.OPEN_PROFILE_TAG) == null) {
-                        openFragment(
-                            fragment = ProfileFragment.newInstance(),
-                            tag = ProfileFragment.OPEN_PROFILE_TAG,
-                            transactionName = PROFILE_TAB
-                        )
-                    }
-                }
-                else -> {}
-            }
-        }
-    }
-
-    private fun setupViewModel() {
-        viewModel.currentTabValue.observe(this) { newTab ->
-            currentTab = newTab
-        }
+    override fun onBackPressed() {
+        bottomNavigator.onBackPressed()
+        super.onBackPressed()
     }
 
     private fun setupDictionariesFragmentResultListeners() {
@@ -86,15 +48,15 @@ class MainActivity : AppCompatActivity() {
         bottomNavigationView?.setOnItemSelectedListener { tab ->
             when (tab.itemId) {
                 R.id.dictionary_tab -> {
-                    selectTab(currentTab, DICTIONARIES_TAB)
+                    bottomNavigator.selectTab(bottomNavigator.getTabByName(DICTIONARIES_TAB))
                     true
                 }
                 R.id.recognition_tab -> {
-                    selectTab(currentTab, RECOGNIZE_TAB)
+                    bottomNavigator.selectTab(bottomNavigator.getTabByName(RECOGNIZE_TAB))
                     true
                 }
                 R.id.profile_tab -> {
-                    selectTab(currentTab, PROFILE_TAB)
+                    bottomNavigator.selectTab(bottomNavigator.getTabByName(PROFILE_TAB))
                     true
                 }
                 else -> false
@@ -106,14 +68,7 @@ class MainActivity : AppCompatActivity() {
         bottomNavigationView = null
     }
 
-    private fun selectTab(currentTab: String, newTab: String) {
-        if (newTab == currentTab) return
-        supportFragmentManager.saveBackStack(currentTab)
-        viewModel.setNewTab(newTab)
-        supportFragmentManager.restoreBackStack(newTab)
-    }
-
-    private fun openFragment(
+    private fun addFragment(
         fragment: Fragment,
         tag: String? = null,
         transactionName: String? = null
@@ -125,9 +80,21 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+    private fun replaceFragment(
+        fragment: Fragment,
+        tag: String? = null,
+        transactionName: String? = null
+    ) {
+        supportFragmentManager.commit {
+            replace(R.id.fragment_container, fragment, tag)
+            addToBackStack(transactionName)
+            setReorderingAllowed(true)
+        }
+    }
+
     companion object {
-        const val DICTIONARIES_TAB = "dictionaries_tab"
-        const val RECOGNIZE_TAB = "recognize_tab"
-        const val PROFILE_TAB = "profile_tab"
+        private const val DICTIONARIES_TAB = "DICTIONARIES"
+        private const val RECOGNIZE_TAB = "RECOGNIZE"
+        private const val PROFILE_TAB = "PROFILE"
     }
 }
