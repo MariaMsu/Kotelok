2e05cdf2b9b44619f8e712376cf1321aa0292d1b
andreyhoco <andreygud2013@gmail.com>
2021-11-04 14:11:14 +0300

    Добавил запросы в DictionaryWordDefCrossRefDao.kt

--- app/src/main/kotlin/com/designdrivendevelopment/kotelok/persistence/daos/DictionaryWordDefCrossRefDao.kt
+++ app/src/main/kotlin/com/designdrivendevelopment/kotelok/persistence/daos/DictionaryWordDefCrossRefDao.kt
@@ -2,10 +2,23 @@ package com.designdrivendevelopment.kotelok.persistence.daos
 
 import androidx.room.Dao
 import androidx.room.Insert
+import androidx.room.Query
 import com.designdrivendevelopment.kotelok.persistence.roomEntities.DictionaryWordDefCrossRef
 
 @Dao
 interface DictionaryWordDefCrossRefDao {
     @Insert
     suspend fun insert(dictionaryWordDefCrossRef: DictionaryWordDefCrossRef)
+
+    @Insert
+    suspend fun insert(dictionaryWordDefCrossRefs: List<DictionaryWordDefCrossRef>)
+
+    @Query(
+        """
+        DELETE
+        FROM dictionary_word_def_cross_refs
+        WHERE ((dict_id = :dictionaryId) AND (word_def_id = :wordDefinitionId))
+        """
+    )
+    suspend fun deleteCrossRefByIds(dictionaryId: Long, wordDefinitionId: Long)
 }
