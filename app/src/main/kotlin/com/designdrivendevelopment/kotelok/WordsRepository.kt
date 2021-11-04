6ec66540e34363d19d4573eeef341e7efea2de68
andreyhoco <andreygud2013@gmail.com>
2021-11-04 15:08:04 +0300

    Изменил набор методов в WordsRepository

--- app/src/main/kotlin/com/designdrivendevelopment/kotelok/WordsRepository.kt
+++ app/src/main/kotlin/com/designdrivendevelopment/kotelok/WordsRepository.kt
@@ -6,16 +6,4 @@ interface WordsRepository {
     suspend fun getAllWords(): List<Word>
 
     suspend fun getWordById(wordId: Long): Word
-
-    suspend fun getWordByWriting(writing: String): Word
-
-    suspend fun addWords(words: List<Word>)
-
-    suspend fun addWord(word: Word)
-
-    suspend fun updateWord(word: Word)
-
-    suspend fun deleteWordById(wordId: Long)
-
-    suspend fun deleteWordsById(wordIds: List<Long>)
 }
