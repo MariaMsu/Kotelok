de81b67c13779ad70e39f7bd43fa8b919593fbf7
andreyhoco <andreygud2013@gmail.com>
2021-11-04 15:05:48 +0300

    Изменил сигнатуры методов DictionariesRepository

--- app/src/main/kotlin/com/designdrivendevelopment/kotelok/DictionariesRepository.kt
+++ app/src/main/kotlin/com/designdrivendevelopment/kotelok/DictionariesRepository.kt
@@ -1,15 +1,24 @@
 package com.designdrivendevelopment.kotelok
 
 import com.designdrivendevelopment.kotelok.entities.Dictionary
+import com.designdrivendevelopment.kotelok.entities.WordDefinition
 
 interface DictionariesRepository {
     suspend fun getAllDictionaries(): List<Dictionary>
 
     suspend fun getDictionaryById(dictionaryId: Long): Dictionary
 
-    suspend fun addDictionary(dictionary: Dictionary)
+    suspend fun addDictionary(
+        dictionary: Dictionary,
+        addedWordDefinitions: List<WordDefinition>? = null
+    )
 
-    suspend fun updateDictionaryBydId(dictionary: Dictionary)
+    suspend fun updateDictionary(dictionary: Dictionary)
 
-    suspend fun deleteDictionaryById(dictionaryId: Long)
+    suspend fun deleteDictionary(dictionary: Dictionary)
+
+    suspend fun deleteWordDefinitionFromDictionary(
+        dictionary: Dictionary,
+        wordDefinition: WordDefinition
+    )
 }
