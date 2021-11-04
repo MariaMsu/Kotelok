0b9ccad3a06f540e54f903fc964030d909c6ce01
andreyhoco <andreygud2013@gmail.com>
2021-11-04 15:49:53 +0300

    Изменил YandexDictionaryResponse.kt
    Убрал nullable у поля definitions, так как в случае пустого ответа оно будет равно пустому списку, а не null

--- app/src/main/kotlin/com/designdrivendevelopment/kotelok/responses/YandexDictionaryResponse.kt
+++ app/src/main/kotlin/com/designdrivendevelopment/kotelok/responses/YandexDictionaryResponse.kt
@@ -6,5 +6,5 @@ import kotlinx.serialization.Serializable
 @Serializable
 data class YandexDictionaryResponse(
     @SerialName("def")
-    val definitions: List<DefinitionResponse>? = null
+    val definitions: List<DefinitionResponse>
 )
