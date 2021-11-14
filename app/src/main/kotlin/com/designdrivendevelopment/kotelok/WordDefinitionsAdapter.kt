40b57f6672368f54d1ec09e8a3bba6c641471a76
andreyhoco <andreygud2013@gmail.com>
2021-11-14 13:55:44 +0300

    Добавил адаптер для List<WordDefinition>

---
+++ app/src/main/kotlin/com/designdrivendevelopment/kotelok/WordDefinitionsAdapter.kt
@@ -0,0 +1,69 @@
+package com.designdrivendevelopment.kotelok
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.designdrivendevelopment.kotelok.entities.WordDefinition
+
+class WordDefinitionsAdapter(
+    private val context: Context,
+    private val wordDefinitions: List<WordDefinition>
+) : RecyclerView.Adapter<WordDefinitionsAdapter.ViewHolder>() {
+
+    inner class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+        private val writingText: TextView = view.findViewById(R.id.writing_text)
+        private val translationText: TextView = view.findViewById(R.id.translation_text)
+        private val originalExampleText: TextView = view.findViewById(R.id.original_example_text)
+        private val translationExampleText: TextView =
+            view.findViewById(R.id.translation_example_text)
+
+        private fun String.capitalize(): String {
+            return this.replaceFirstChar { firstChar ->
+                if (firstChar.isLowerCase()) firstChar.titlecase() else firstChar.toString()
+            }
+        }
+
+        fun bind(definition: WordDefinition) {
+            writingText.text = definition.writing.capitalize()
+            if (definition.partOfSpeech != null) {
+                translationText.text = context.resources.getString(
+                    R.string.translation_pos_line,
+                    definition.mainTranslation.capitalize(),
+                    definition.partOfSpeech
+                )
+            } else {
+                translationText.text = definition.mainTranslation.capitalize()
+            }
+            if (definition.examples.isNotEmpty()) {
+                val mainExample = definition.examples.first()
+                originalExampleText.text = mainExample.originalText.capitalize()
+                if (mainExample.translatedText.isNotEmpty()) {
+                    translationExampleText.text = mainExample.translatedText.capitalize()
+                } else {
+                    translationExampleText.visibility = View.GONE
+                }
+            } else {
+                originalExampleText.visibility = View.GONE
+                translationExampleText.visibility = View.GONE
+            }
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val layoutInflater = LayoutInflater.from(context)
+        return ViewHolder(
+            layoutInflater.inflate(R.layout.item_word_definition, parent, false)
+        )
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.bind(wordDefinitions[position])
+    }
+
+    override fun getItemCount(): Int {
+        return wordDefinitions.size
+    }
+}
