65f5ba0a893adcfc9b78c15cafe1673dcf8a2149
andreyhoco <andreygud2013@gmail.com>
2021-11-17 01:36:06 +0300

    Добавил методы в WordDefinitionsDao.kt
    Добавил метод для частичного обновления информации об определении слова, а также метод для удаления списка слов

--- app/src/main/kotlin/com/designdrivendevelopment/kotelok/persistence/daos/WordDefinitionsDao.kt
+++ app/src/main/kotlin/com/designdrivendevelopment/kotelok/persistence/daos/WordDefinitionsDao.kt
@@ -4,6 +4,7 @@ import androidx.room.Dao
 import androidx.room.Insert
 import androidx.room.Query
 import androidx.room.Transaction
+import com.designdrivendevelopment.kotelok.entities.Language
 import com.designdrivendevelopment.kotelok.persistence.queryResults.WordDefinitionQueryResult
 import com.designdrivendevelopment.kotelok.persistence.roomEntities.WordDefinitionEntity
 
@@ -11,6 +12,28 @@ import com.designdrivendevelopment.kotelok.persistence.roomEntities.WordDefiniti
 interface WordDefinitionsDao {
     @Insert
     suspend fun insert(wordDefinitionEntity: WordDefinitionEntity): Long
+
+    // В данной функции нельзя уменьшить число аргументов, т. к. она реализует SQL-запрос
+    @Suppress("LongParameterList")
+    @Query(
+        """
+        UPDATE word_definitions
+        SET writing = :writing,
+            language = :language,
+            part_of_speech = :partOfSpeech,
+            transcription = :transcription,
+            main_translation =:mainTranslation
+        WHERE (def_id = :wordDefinitionId)
+    """
+    )
+    suspend fun updateWordDefinitionAttributes(
+        wordDefinitionId: Long,
+        writing: String,
+        language: Language,
+        partOfSpeech: String?,
+        transcription: String?,
+        mainTranslation: String,
+    )
 
     @Transaction
     @Query(
@@ -61,4 +84,7 @@ interface WordDefinitionsDao {
 
     @Query("DELETE FROM word_definitions WHERE (def_id = :wordDefinitionId)")
     suspend fun deleteWordDefinitionById(wordDefinitionId: Long)
+
+    @Query("DELETE FROM word_definitions WHERE (def_id IN (:wordDefinitionIds))")
+    suspend fun deleteWordDefinitionsByIds(wordDefinitionIds: List<Long>)
 }
