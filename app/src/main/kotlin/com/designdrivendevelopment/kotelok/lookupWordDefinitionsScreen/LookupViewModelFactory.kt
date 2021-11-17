08b5a0516f23cefe0ce998eb59f425ba6f011699
andreyhoco <andreygud2013@gmail.com>
2021-11-17 15:33:52 +0300

    Добавил factory для LookupViewModel

---
+++ app/src/main/kotlin/com/designdrivendevelopment/kotelok/lookupWordDefinitionsScreen/LookupViewModelFactory.kt
@@ -0,0 +1,17 @@
+package com.designdrivendevelopment.kotelok.lookupWordDefinitionsScreen
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import com.designdrivendevelopment.kotelok.EditWordDefinitionsRepository
+
+class LookupViewModelFactory(
+    private val editWordDefinitionsRepository: EditWordDefinitionsRepository
+) : ViewModelProvider.Factory {
+    @Suppress("UNCHECKED_CAST")
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        if (modelClass.isAssignableFrom(LookupViewModel::class.java)) {
+            return LookupViewModel(editWordDefinitionsRepository) as T
+        }
+        throw IllegalArgumentException("Unknown ViewModel class")
+    }
+}
