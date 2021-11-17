7848a3510c971321f1cc0c4c340a14e3e6dc45cd
andreyhoco <andreygud2013@gmail.com>
2021-11-17 11:12:40 +0300

    Реализовал остальные методы для работы с определениями

--- app/src/main/kotlin/com/designdrivendevelopment/kotelok/EditWordDefRepositoryImpl.kt
+++ app/src/main/kotlin/com/designdrivendevelopment/kotelok/EditWordDefRepositoryImpl.kt
@@ -7,6 +7,10 @@ import com.designdrivendevelopment.kotelok.persistence.daos.ExamplesDao
 import com.designdrivendevelopment.kotelok.persistence.daos.SynonymsDao
 import com.designdrivendevelopment.kotelok.persistence.daos.TranslationsDao
 import com.designdrivendevelopment.kotelok.persistence.daos.WordDefinitionsDao
+import com.designdrivendevelopment.kotelok.persistence.roomEntities.DictionaryWordDefCrossRef
+import com.designdrivendevelopment.kotelok.persistence.roomEntities.ExampleEntity
+import com.designdrivendevelopment.kotelok.persistence.roomEntities.SynonymEntity
+import com.designdrivendevelopment.kotelok.persistence.roomEntities.TranslationEntity
 import com.designdrivendevelopment.kotelok.yandexDictApi.YandexDictionaryApiService
 import com.designdrivendevelopment.kotelok.yandexDictApi.responses.YandexDictionaryResponse
 import java.net.UnknownHostException
@@ -51,25 +55,139 @@ class EditWordDefRepositoryImpl(
     }
 
     override suspend fun getSavedDefinitionsByWriting(writing: String): List<WordDefinition> {
-        TODO("Not yet implemented")
+        return wordDefinitionsDao.getDefinitionsByWriting(writing)
+            .map { wordDefinitionQueryResult ->
+                wordDefinitionQueryResult.toWordDefinition()
+            }
     }
 
     override suspend fun addWordDefinition(wordDefinition: WordDefinition) {
-        TODO("Not yet implemented")
+        if (wordDefinition.id == NEW_WORD_ID) {
+            addNewWordDefinition(wordDefinition)
+        } else {
+            updateWordDefinition(wordDefinition)
+        }
     }
 
     override suspend fun addNewWordDefinitionWithDictionaries(
         wordDefinition: WordDefinition,
-        dictionaries: List<Dictionary>,
+        dictionaries: List<Dictionary>
     ) {
-        TODO("Not yet implemented")
+        if (wordDefinition.id == NEW_WORD_ID) {
+            addNewWordDefinition(wordDefinition, dictionaries)
+        } else {
+            updateWordDefinition(wordDefinition, dictionaries)
+        }
     }
 
     override suspend fun deleteWordDefinition(wordDefinition: WordDefinition) {
-        TODO("Not yet implemented")
+        wordDefinitionsDao.deleteWordDefinitionById(wordDefinition.id)
     }
 
     override suspend fun deleteWordDefinitions(wordDefinitions: List<WordDefinition>) {
-        TODO("Not yet implemented")
+        wordDefinitionsDao.deleteWordDefinitionsByIds(wordDefinitions.map { it.id })
+    }
+
+    private suspend fun addNewWordDefinition(
+        wordDefinition: WordDefinition,
+        dictionaries: List<Dictionary>? = null
+    ): Long {
+        val definitionId = wordDefinitionsDao.insert(wordDefinition.toWordDefinitionEntity())
+        val synonymEntities = wordDefinition.synonyms.map { synonym ->
+            SynonymEntity(
+                wordDefinitionId = definitionId,
+                writing = synonym
+            )
+        }
+        val translationEntities = wordDefinition.allTranslations.map { translation ->
+            TranslationEntity(
+                wordDefinitionId = definitionId,
+                translation = translation
+            )
+        }
+        val exampleEntities = wordDefinition.examples.map { exampleOfDefinitionUse ->
+            ExampleEntity(
+                wordDefinitionId = definitionId,
+                original = exampleOfDefinitionUse.originalText,
+                translation = exampleOfDefinitionUse.translatedText
+            )
+        }
+
+        synonymsDao.insert(synonymEntities)
+        translationsDao.insert(translationEntities)
+        examplesDao.insert(exampleEntities)
+
+        if (dictionaries != null) {
+            addCrossRefs(definitionId, dictionaries)
+        }
+        return definitionId
+    }
+
+    private suspend fun updateWordDefinition(
+        wordDefinition: WordDefinition,
+        dictionaries: List<Dictionary>? = null
+    ) {
+        wordDefinitionsDao.updateWordDefinitionAttributes(
+            wordDefinitionId = wordDefinition.id,
+            writing = wordDefinition.writing,
+            language = wordDefinition.language,
+            partOfSpeech = wordDefinition.partOfSpeech,
+            transcription = wordDefinition.transcription,
+            mainTranslation = wordDefinition.mainTranslation
+        )
+
+        val synonymEntities = wordDefinition.synonyms.map { synonym ->
+            SynonymEntity(
+                wordDefinitionId = wordDefinition.id,
+                writing = synonym
+            )
+        }
+        val translationEntities = wordDefinition.allTranslations.map { translation ->
+            TranslationEntity(
+                wordDefinitionId = wordDefinition.id,
+                translation = translation
+            )
+        }
+        val exampleEntities = wordDefinition.examples.map { exampleOfDefinitionUse ->
+            ExampleEntity(
+                wordDefinitionId = wordDefinition.id,
+                original = exampleOfDefinitionUse.originalText,
+                translation = exampleOfDefinitionUse.translatedText
+            )
+        }
+
+        synonymsDao.insert(synonymEntities)
+        synonymsDao.deleteRedundantSynonyms(wordDefinition.id, wordDefinition.synonyms)
+
+        translationsDao.insert(translationEntities)
+        translationsDao.deleteRedundantTranslations(wordDefinition.id, wordDefinition.allTranslations)
+
+        examplesDao.insert(exampleEntities)
+        examplesDao.deleteRedundantExamples(
+            wordDefinition.id,
+            wordDefinition.examples.map { it.originalText }
+        )
+
+        if (dictionaries != null) {
+            addCrossRefs(wordDefinition.id, dictionaries)
+            dictWordDefCrossRefDao.deleteRedundantCrossRefsById(
+                newDictionariesIds = dictionaries.map { it.id },
+                wordDefinitionId = wordDefinition.id
+            )
+        }
+    }
+
+    private suspend fun addCrossRefs(definitionId: Long, dictionaries: List<Dictionary>) {
+        val crossRefs = dictionaries.map { dictionary ->
+            DictionaryWordDefCrossRef(
+                dictionaryId = dictionary.id,
+                wordDefinitionId = definitionId
+            )
+        }
+        dictWordDefCrossRefDao.insert(crossRefs)
+    }
+
+    companion object {
+        private const val NEW_WORD_ID = 0L
     }
 }
