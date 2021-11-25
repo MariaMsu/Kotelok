93d190160f923ca7942fb8299f128ed936510ebc
Alexandra Pervushina <ficstula@gmail.com>
2021-11-25 23:40:16 +0300

    Попрвила LiveData

--- app/src/main/kotlin/com/designdrivendevelopment/kotelok/TrainFlashcardsFragment.kt
+++ app/src/main/kotlin/com/designdrivendevelopment/kotelok/TrainFlashcardsFragment.kt
@@ -12,7 +12,7 @@ import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 
 class TrainFlashcardsFragment : Fragment() {
-    var viewModel: TrainFlashcardsViewModel? = null
+    lateinit var viewModel: TrainFlashcardsViewModel
 
     private var yesButton: ImageButton? = null
     private var noButton: ImageButton? = null
@@ -21,54 +21,48 @@ class TrainFlashcardsFragment : Fragment() {
     private var textCompleted: TextView? = null
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        viewModel?.currentWord?.observe(
-            viewLifecycleOwner,
-            Observer {
-                newWord ->
-                updateFlashcard()
-            }
-        )
-        viewModel?.state?.observe(
-            viewLifecycleOwner,
-            Observer {
-                newState ->
-                if (viewModel?.state?.value != State.NOT_GUESSED) {
-                    updateButtonVisibility(true)
-                } else {
-                    updateButtonVisibility(false)
-                }
-            }
-        )
-        return inflater.inflate(R.layout.fragment_train_flashcards, container, false)
-    }
+        val binding = inflater.inflate(R.layout.fragment_train_flashcards, container, false)
 
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
+        word = binding.findViewById(R.id.Word)
+        textCompleted = binding.findViewById(R.id.textCompleted)
+        flashcardButton = binding.findViewById(R.id.FlashcardButton)
+        flashcardButton?.setOnClickListener(listener)
+        yesButton = binding.findViewById(R.id.YesButton)
+        yesButton?.setOnClickListener(listener)
+        noButton = binding.findViewById(R.id.NoButton)
+        noButton?.setOnClickListener(listener)
+
         val dictionaryId = arguments?.getLong("id") ?: 1
-
         val factory = TrainFlashcardsViewModelFactory(
             dictionaryId,
             (requireActivity().application as KotelokApplication)
                 .appComponent.cardsLearnDefRepository
         )
         viewModel = ViewModelProvider(this, factory).get(TrainFlashcardsViewModel::class.java)
-
-        word = view.findViewById(R.id.Word)
-        textCompleted = view.findViewById(R.id.textCompleted)
-        flashcardButton = view.findViewById(R.id.FlashcardButton)
-        flashcardButton?.setOnClickListener(listener)
-        yesButton = view.findViewById(R.id.YesButton)
-        yesButton?.setOnClickListener(listener)
-        noButton = view.findViewById(R.id.NoButton)
-        noButton?.setOnClickListener(listener)
-
-        // UpdateFlashcard()
+        viewModel.viewState.observe(
+            viewLifecycleOwner,
+            {
+            updateFlashcard()
+            if (viewModel.viewState.value != State.NOT_GUESSED) {
+                    updateButtonVisibility(true)
+                } else {
+                    updateButtonVisibility(false)
+                }
+            }
+        )
+        viewModel.currentWord.observe(
+            viewLifecycleOwner,
+            {
+                updateFlashcard()
+            }
+        )
+        return binding
     }
 
     private val listener = View.OnClickListener { view ->
         when (view.id) {
             R.id.FlashcardButton -> {
-                pressFlashcard()
+                viewModel.onCardPressed(viewModel.viewState.value!!)
             }
             R.id.YesButton -> {
                 pressGuessButton(true)
@@ -86,34 +80,18 @@ class TrainFlashcardsFragment : Fragment() {
         noButton?.isClickable = isActive
     }
 
-    private fun pressFlashcard() {
-        viewModel?.state?.value = when (viewModel?.state?.value) {
-            State.NOT_GUESSED -> State.GUESSED_TRANSLATION
-            State.GUESSED_TRANSLATION -> State.GUESSED_WORD
-            State.GUESSED_WORD -> State.GUESSED_TRANSLATION
-            else -> State.NOT_GUESSED
-        }
-        // if (viewModel?.state?.value != State.NOT_GUESSED) {
-        //    updateButtonVisibility(true)
-        // }
-        // UpdateFlashcard()
-    }
-
     private fun updateFlashcard() {
-        word?.text = when (viewModel?.state?.value) {
-            State.NOT_GUESSED -> viewModel?.currentWord?.value?.writing
-            State.GUESSED_TRANSLATION -> viewModel?.currentWord?.value?.mainTranslation
-            State.GUESSED_WORD -> viewModel?.currentWord?.value?.writing
+        word?.text = when (viewModel.viewState.value) {
+            State.NOT_GUESSED -> viewModel.currentWord.value?.writing
+            State.GUESSED_TRANSLATION -> viewModel.currentWord.value?.mainTranslation
+            State.GUESSED_WORD -> viewModel.currentWord.value?.writing
             else -> "Error"
         }
     }
 
     private fun pressGuessButton(guess: Boolean) {
-        viewModel?.state?.value = State.NOT_GUESSED
-        // updateButtonVisibility(false)
-        viewModel?.trainerCards?.checkUserInput(guess)
-        if (!viewModel?.trainerCards?.isDone!!) {
-            viewModel?.currentWord?.value = viewModel?.trainerCards?.getNext()
+        viewModel.onGuessPressed(guess)
+        if (!viewModel.trainerCards.isDone) {
             updateFlashcard()
         } else {
             textCompleted?.isVisible = true
--- app/src/main/kotlin/com/designdrivendevelopment/kotelok/TrainFlashcardsViewModel.kt
+++ app/src/main/kotlin/com/designdrivendevelopment/kotelok/TrainFlashcardsViewModel.kt
@@ -1,33 +1,45 @@
 package com.designdrivendevelopment.kotelok
 
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.designdrivendevelopment.kotelok.entities.LearnableDefinition
 import com.designdrivendevelopment.kotelok.trainer.TrainerCards
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 
 class TrainFlashcardsViewModel(
     dictionaryId: Long,
     cardsLearnDefRepository: CardsLearnableDefinitionsRepository
 ) : ViewModel() {
-    var state: MutableLiveData<TrainFlashcardsFragment.State> = MutableLiveData<TrainFlashcardsFragment.State>()
-    var trainerCards: TrainerCards? = null
-    var currentWord: MutableLiveData<LearnableDefinition>? = null
+    private val _viewState = MutableLiveData(TrainFlashcardsFragment.State.NOT_GUESSED)
+    val trainerCards: TrainerCards = TrainerCards(cardsLearnDefRepository)
+    val viewState: LiveData<TrainFlashcardsFragment.State> = _viewState
+    private val _currentWord: MutableLiveData<LearnableDefinition> = MutableLiveData()
+    val currentWord: LiveData<LearnableDefinition> = _currentWord
 
     init {
-        state.value = TrainFlashcardsFragment.State.NOT_GUESSED
-        loadTrainerCardsById(dictionaryId, cardsLearnDefRepository)
+        viewModelScope.launch(Dispatchers.IO) {
+            trainerCards.loadDictionary(dictionaryId, onlyNotLearned = true)
+            val learnableDefinition = trainerCards.getNext()
+            _currentWord.postValue(learnableDefinition)
+        }
     }
 
-    fun loadTrainerCardsById(dictionaryId: Long, cardsLearnDefRepository: CardsLearnableDefinitionsRepository) {
-        trainerCards = TrainerCards(cardsLearnDefRepository)
-        viewModelScope.launch {
-            trainerCards?.loadDictionary(
-                dictionaryId,
-                false
-            )
-            currentWord?.value = trainerCards?.getNext()
+    fun onCardPressed(currentState: TrainFlashcardsFragment.State) {
+        _viewState.value = when (currentState) {
+            TrainFlashcardsFragment.State.NOT_GUESSED -> TrainFlashcardsFragment.State.GUESSED_TRANSLATION
+            TrainFlashcardsFragment.State.GUESSED_TRANSLATION -> TrainFlashcardsFragment.State.GUESSED_WORD
+            TrainFlashcardsFragment.State.GUESSED_WORD -> TrainFlashcardsFragment.State.GUESSED_TRANSLATION
+        }
+    }
+
+    fun onGuessPressed(guess: Boolean) {
+        _viewState.value = TrainFlashcardsFragment.State.NOT_GUESSED
+        trainerCards.checkUserInput(guess)
+        if (!trainerCards.isDone) {
+            _currentWord.value = trainerCards.getNext()
         }
     }
 }
