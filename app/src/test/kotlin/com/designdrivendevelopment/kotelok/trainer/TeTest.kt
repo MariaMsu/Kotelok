b73a1574452774f9d51f9f970c5cbec46d6f92b2
MariaMsu <i.knaz@yandex.ru>
2021-12-03 23:05:30 +0300

    Тест с корутинами (черновик)

---
+++ app/src/test/kotlin/com/designdrivendevelopment/kotelok/trainer/TeTest.kt
@@ -0,0 +1,24 @@
+package com.designdrivendevelopment.kotelok.trainer
+
+import com.designdrivendevelopment.kotelok.entities.LearnableDefinition
+import junit.framework.TestCase.assertEquals
+import junit.framework.TestCase.assertTrue
+import kotlinx.coroutines.runBlocking
+import org.junit.Test
+
+class TeTest {
+    private val mockLDR = MockLDR()
+
+
+    @Test
+    fun cardsTest() = runBlocking{
+        val data = mockLDR.getAll()
+        val trainer = TrainerCards(learnableDefinitionsRepository=mockLDR)
+//        assertEquals(data.size, trainer.size)
+        while (!trainer.isDone){
+            val learnableDefinition : LearnableDefinition = trainer.getNext()
+            val correctUserInput = true
+            assertTrue(trainer.checkUserInput(correctUserInput))
+        }
+    }
+}
