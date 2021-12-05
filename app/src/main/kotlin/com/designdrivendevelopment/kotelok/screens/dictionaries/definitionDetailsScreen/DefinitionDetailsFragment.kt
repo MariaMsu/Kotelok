ad71d8944c0033e4437f6fa9a8999f44c166c1a8
andreyhoco <andreygud2013@gmail.com>
2021-12-05 15:42:15 +0300

    Добавил ссылки на view в DefinitionDetailsFragment.kt

--- app/src/main/kotlin/com/designdrivendevelopment/kotelok/screens/dictionaries/definitionDetailsScreen/DefinitionDetailsFragment.kt
+++ app/src/main/kotlin/com/designdrivendevelopment/kotelok/screens/dictionaries/definitionDetailsScreen/DefinitionDetailsFragment.kt
@@ -4,12 +4,30 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import androidx.constraintlayout.helper.widget.Flow
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.RecyclerView
 import com.designdrivendevelopment.kotelok.R
 import com.designdrivendevelopment.kotelok.application.KotelokApplication
+import com.google.android.material.textfield.TextInputLayout
 
 class DefinitionDetailsFragment : Fragment() {
+    private var writingField: TextInputLayout? = null
+    private var translationField: TextInputLayout? = null
+    private var transcriptionField: TextInputLayout? = null
+    private var partOfSpeechField: TextInputLayout? = null
+    private var translationsList: RecyclerView? = null
+    private var synonymsList: RecyclerView? = null
+    private var examplesList: RecyclerView? = null
+    private var addTranslationBtn: Button? = null
+    private var addSynonymBtn: Button? = null
+    private var addExampleBtn: Button? = null
+    private var translationsGroup: Flow? = null
+    private var synonymsGroup: Flow? = null
+    private var examplesGroup: Flow? = null
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -20,6 +38,8 @@ class DefinitionDetailsFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        initViews(view)
         val dictionaryId = arguments?.getLong(DICT_ID_KEY) ?: DEFAULT_DICT_ID
         val saveMode = arguments?.getInt(SAVE_MODE_KEY) ?: SAVE_MODE_COPY
 
@@ -35,6 +55,43 @@ class DefinitionDetailsFragment : Fragment() {
                 .appComponent.sharedWordDefProvider
         )
         val viewModel = ViewModelProvider(this, factory)[DefDetailsViewModel::class.java]
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        clearViews()
+    }
+
+    private fun initViews(view: View) {
+        writingField = view.findViewById(R.id.writing_text_field)
+        translationField = view.findViewById(R.id.translation_text_field)
+        transcriptionField = view.findViewById(R.id.transcription_text_field)
+        partOfSpeechField = view.findViewById(R.id.part_of_speech_text_field)
+        translationsList = view.findViewById(R.id.other_translations)
+        synonymsList = view.findViewById(R.id.synonyms)
+        examplesList = view.findViewById(R.id.examples)
+        addTranslationBtn = view.findViewById(R.id.add_translation_button)
+        addSynonymBtn = view.findViewById(R.id.add_synonym_button)
+        addExampleBtn = view.findViewById(R.id.add_example_button)
+        translationsGroup = view.findViewById(R.id.other_translations_group)
+        synonymsGroup = view.findViewById(R.id.synonyms_group)
+        examplesGroup = view.findViewById(R.id.examples_group)
+    }
+
+    private fun clearViews() {
+        writingField = null
+        translationField = null
+        transcriptionField = null
+        partOfSpeechField = null
+        translationsList = null
+        synonymsList = null
+        examplesList = null
+        addTranslationBtn = null
+        addSynonymBtn = null
+        addExampleBtn = null
+        translationsGroup = null
+        synonymsGroup = null
+        examplesGroup = null
     }
 
     companion object {
