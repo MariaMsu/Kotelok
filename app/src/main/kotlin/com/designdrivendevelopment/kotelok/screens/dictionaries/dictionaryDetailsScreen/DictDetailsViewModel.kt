794683b3bb175ecf46db9c04854fd66f5dc36efa
andreyhoco <andreygud2013@gmail.com>
2021-12-07 10:46:46 +0300

    Добавил возможность открытия DefDetailsFragment из DictionaryDetailsFragment

--- app/src/main/kotlin/com/designdrivendevelopment/kotelok/screens/dictionaries/dictionaryDetailsScreen/DictDetailsViewModel.kt
+++ app/src/main/kotlin/com/designdrivendevelopment/kotelok/screens/dictionaries/dictionaryDetailsScreen/DictDetailsViewModel.kt
@@ -5,12 +5,14 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.designdrivendevelopment.kotelok.entities.WordDefinition
+import com.designdrivendevelopment.kotelok.screens.sharedWordDefProvider.SharedWordDefinitionProvider
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 
 class DictDetailsViewModel(
     dictionaryId: Long,
-    private val dictWordDefinitionsRepository: DictionaryWordDefinitionsRepository
+    private val dictWordDefinitionsRepository: DictionaryWordDefinitionsRepository,
+    private val sharedWordDefinitionProvider: SharedWordDefinitionProvider
 ) : ViewModel() {
     private val _dictionaryDefinitions = MutableLiveData<List<WordDefinition>>(emptyList())
     val dictionaryDefinitions: LiveData<List<WordDefinition>> = _dictionaryDefinitions
@@ -25,5 +27,9 @@ class DictDetailsViewModel(
                 .getDefinitionsByDictionaryId(dictionaryId)
             _dictionaryDefinitions.postValue(definitions)
         }
+    }
+
+    fun setDisplayedDefinition(definition: WordDefinition?) {
+        sharedWordDefinitionProvider.sharedWordDefinition = definition
     }
 }
--- app/src/main/kotlin/com/designdrivendevelopment/kotelok/screens/dictionaries/dictionaryDetailsScreen/DictDetailsViewModelFactory.kt
+++ app/src/main/kotlin/com/designdrivendevelopment/kotelok/screens/dictionaries/dictionaryDetailsScreen/DictDetailsViewModelFactory.kt
@@ -2,15 +2,21 @@ package com.designdrivendevelopment.kotelok.screens.dictionaries.dictionaryDetai
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
+import com.designdrivendevelopment.kotelok.screens.sharedWordDefProvider.SharedWordDefinitionProvider
 
 class DictDetailsViewModelFactory(
     private val dictionaryId: Long,
-    private val dictWordDefinitionsRepository: DictionaryWordDefinitionsRepository
+    private val dictWordDefinitionsRepository: DictionaryWordDefinitionsRepository,
+    private val sharedWordDefinitionProvider: SharedWordDefinitionProvider
 ) : ViewModelProvider.Factory {
     @Suppress("UNCHECKED_CAST")
     override fun <T : ViewModel> create(modelClass: Class<T>): T {
         if (modelClass.isAssignableFrom(DictDetailsViewModel::class.java)) {
-            return DictDetailsViewModel(dictionaryId, dictWordDefinitionsRepository) as T
+            return DictDetailsViewModel(
+                dictionaryId,
+                dictWordDefinitionsRepository,
+                sharedWordDefinitionProvider
+            ) as T
         }
         throw IllegalArgumentException("Unknown ViewModel class")
     }
