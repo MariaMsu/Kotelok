932f2a8ec6b80d4cfe319a26fae2c5b2c9bf8028
andreyhoco <andreygud2013@gmail.com>
2021-12-08 03:08:16 +0300

    Добавил AddDictViewModel

---
+++ app/src/main/kotlin/com/designdrivendevelopment/kotelok/screens/dictionaries/addDictionaryScreen/AddDictViewModel.kt
@@ -0,0 +1,75 @@
+package com.designdrivendevelopment.kotelok.screens.dictionaries.addDictionaryScreen
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.designdrivendevelopment.kotelok.entities.Dictionary
+import com.designdrivendevelopment.kotelok.entities.WordDefinition
+import com.designdrivendevelopment.kotelok.screens.dictionaries.DictionariesRepository
+import com.designdrivendevelopment.kotelok.screens.dictionaries.dictionaryDetailsScreen.DictionaryWordDefinitionsRepository
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+
+class AddDictViewModel(
+    private val dictionariesRepository: DictionariesRepository,
+    private val dictDefinitionsRepository: DictionaryWordDefinitionsRepository
+) : ViewModel() {
+    private val _allDefinitions: MutableLiveData<List<SelectableWordDefinition>> = MutableLiveData()
+    private var unfilteredDefinitions: List<SelectableWordDefinition> = emptyList()
+    private var selectedDefinitions: MutableList<WordDefinition> = mutableListOf()
+    val allDefinitions: LiveData<List<SelectableWordDefinition>>
+        get() = _allDefinitions
+
+    init {
+        loadAllDefinitions()
+    }
+
+    fun loadAllDefinitions() {
+        viewModelScope.launch(Dispatchers.IO) {
+            unfilteredDefinitions = dictDefinitionsRepository.getAllDefinitions()
+                .map { SelectableWordDefinition(it) }
+            _allDefinitions.postValue(unfilteredDefinitions)
+        }
+    }
+
+    fun changeItemSelection(definition: WordDefinition, isSelected: Boolean) {
+        if (isSelected) {
+            selectedDefinitions.add(definition)
+        } else {
+            selectedDefinitions.remove(definition)
+        }
+        unfilteredDefinitions = unfilteredDefinitions.map { selectableDefinition ->
+            if (selectableDefinition.def == definition) {
+                selectableDefinition.copy(isSelected = isSelected)
+            } else {
+                selectableDefinition
+            }
+        }
+    }
+
+    fun filter(text: String) {
+        if (text.isEmpty()) {
+            _allDefinitions.value = unfilteredDefinitions
+        } else {
+            val filteredDictionaries = unfilteredDefinitions.filter { selectableDefinition ->
+                selectableDefinition.def.writing.startsWith(text, ignoreCase = true)
+            }
+            _allDefinitions.value = filteredDictionaries
+        }
+    }
+
+    fun addDictionary(label: String) {
+        viewModelScope.launch(Dispatchers.IO) {
+            dictionariesRepository.addDictionary(
+                Dictionary(
+                    id = Dictionary.NEW_DICTIONARY_ID,
+                    label = label,
+                    size = Dictionary.SIZE_EMPTY,
+                    isFavorite = false
+                ),
+                selectedDefinitions
+            )
+        }
+    }
+}
