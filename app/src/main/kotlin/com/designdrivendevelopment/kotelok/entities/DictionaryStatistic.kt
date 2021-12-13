b05cccd22ff5e5e3f3a89467c11758a388a15d1a
andreyhoco <andreygud2013@gmail.com>
2021-12-13 11:53:57 +0300

    Добавил новые сущности для статистики
    AnswersStatistic - для статистики по работе тренажерами
    DictionaryStatistic - для статистики по словарям

---
+++ app/src/main/kotlin/com/designdrivendevelopment/kotelok/entities/DictionaryStatistic.kt
@@ -0,0 +1,8 @@
+package com.designdrivendevelopment.kotelok.entities
+
+data class DictionaryStatistic(
+    val id: Long,
+    val label: String,
+    val averageSkillLevel: Float,
+    val size: Int
+)
